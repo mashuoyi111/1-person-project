9412654d2e9d5a2fc9459edb270b0ef77ae57f8b
Max Ma <mashuoyi111@sina.com>
2017-01-10 16:56:39 -0800

    new game activity and png poker cards pictures from Internet

---
+++ app/src/main/java/com/example/administrator/a1_person_project/MainGame.java
@@ -0,0 +1,19 @@
+package com.example.administrator.a1_person_project;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/1/10.
+ */
+
+public class MainGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_main);
+    }
+
+
+}
